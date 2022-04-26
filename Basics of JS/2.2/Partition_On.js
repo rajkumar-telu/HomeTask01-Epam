2b020439f38b19d09ea028cc47ca8ae315e3377b
Rajkumar_Telukuntla <rajkumar_telukuntla@epam.com>
2022-04-26 19:07:20 +0530

    Solution without using splice

--- Basics of JS/2.2/Partition_On.js
+++ Basics of JS/2.2/Partition_On.js
@@ -1,13 +1,29 @@
 // partition the items array so that all values for which pred returns true are
 // at the end, returning the index of the first true value
 function partitionOn(pred, items) {
-    var res=0;
-    for(key in items){
-      if(!pred(items[key])){  
-        val = items[key]    
-        items.splice(key,1) 
-        items.splice(res++,0,val)
+    var temp = [],temp1 = [];
+    items.forEach(element => {
+      if(!pred(element)){
+        temp.push(element);
       }
+      else{
+        temp1.push(element);
+      }
+    });
+    for(var i = 0;i<temp.length;i++){
+      items[i] = temp[i];
     }
-    return res;
-  }
+    for(var j = 0;j<temp1.length;j++){
+      items[i+j] = temp1[j];
+    }
+    return temp.length;
+    //var res = 0;
+    // for(key in items){
+    //   if(!pred(items[key])){  
+    //     val = items[key]    
+    //     items.splice(key,1) 
+    //     items.splice(res++,0,val)
+    //   }
+    // }
+    // return res;
+}
