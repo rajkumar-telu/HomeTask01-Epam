9980056ebdedc34c586e5ad4732eb77446785d60
Rajkumar_Telukuntla <rajkumar_telukuntla@epam.com>
2022-05-05 12:04:47 +0530

    Converted to arrow functions

--- Basics of JS/2.5/Array_Helpers.js
+++ Basics of JS/2.5/Array_Helpers.js
@@ -1,23 +1,23 @@
 Array.prototype.sum = function(){
-    return this.reduce(function(a,b) { return a+b; },0)
-  }
-  
-  Array.prototype.square = function(){
-    return this.map(function(n){ return n*n});
-  }
-  
-  Array.prototype.cube = function(){
-    return this.map(function(n){ return n*n*n});
-  }
-  
-  Array.prototype.even = function(){
-    return this.filter(function(n){ return n%2==0});
-  }
-  
-  Array.prototype.odd = function(){
-    return this.filter(function(n){ return n%2==1});
-  }
-  
-  Array.prototype.average = function(){
-    return this.sum()/this.length;
-  }
+  return this.reduce((a,b) => a+b,0)
+}
+
+Array.prototype.square = function(){
+  return this.map((n) => n*n);
+}
+
+Array.prototype.cube = function(){
+  return this.map((n) => n*n*n);
+}
+
+Array.prototype.even = function(){
+  return this.filter((n) => n%2==0);
+}
+
+Array.prototype.odd = function(){
+  return this.filter((n) => n%2==1);
+}
+
+Array.prototype.average = function(){
+  return this.sum()/this.length;
+}
